4ad5a6c932c78a552b4454c584b8176b35190d46
JosefForkman <strand.vatten@outlook.com>
2022-10-24 20:37:06 +0100

    the beginning of the navigation bar

--- index.php
+++ index.php
@@ -3,9 +3,29 @@
     <head>
         <meta charset='UTF-8'>
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <script src="https://kit.fontawesome.com/a913f4ac89.js" crossorigin="anonymous"></script>
         <link rel='stylesheet' href='css/style.css'>
         <title></title>
     </head>
     <body>
+        <header>
+            <p>Logga här</p>
+            <div class="hamburger"></div>
+            <nav>
+                <i class="fa-solid fa-x" style="color: var(--Deep-champagne); font-size: 2rem; cursor: pointer;"></i>
+                <ul>
+                    <li><a href="index.php">Hem</a></li>
+                    <li><a href="#">Alla recept</a></li>
+                    <li><a href="#">Om oss</a></li>
+                    <li><a href="#">Kontakta oss</a></li>
+                    <li><a href="#">Logga in</a></li>
+                    <li><a href="#">Registrera</a></li>
+                </ul>
+            </nav>
+        </header>
+        <main>
+            <img src="image/anna-pelzer-IGfIGP5ONV0-unsplash.jpg" alt="">
+            <h2>Varför ska man välja Kok boken.</h2>
+        </main>
     </body>
 </html>
