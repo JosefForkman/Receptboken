6e3163b37fd616bb3f5e513e126cee1d981a1615
JosefForkman <strand.vatten@outlook.com>
2022-10-26 20:28:00 +0100

    start of home page

--- index.php
+++ index.php
@@ -24,8 +24,35 @@
             </nav>
         </header>
         <main class="pageMargin">
-            <img src="image/anna-pelzer-IGfIGP5ONV0-unsplash.jpg" alt="">
-            <h2>Varför ska man välja Kok boken.</h2>
+            <section id="">
+                <img src="image/anna-pelzer-IGfIGP5ONV0-unsplash.jpg" alt="">
+                <h2>Varför ska man välja Kok boken.</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Massa ipsum lorem massa risus sed etiam. Ullamcorper pellentesque viverra viverra massa nulla. Habitant integer posuere eget donec commodo risus euismod. Et et tristique.</p>
+                <a href="#" class="btn bg-TerraCotta">Se alla recept</a>
+            </section>
+
+            <section id="Posts">
+                <div class="post">
+                    <h2 class="text-GreenSheen">Ungnspankaka</h2>
+                    <p>Saftig och luftig ugnspannkaka när den är som bäst. Enkelt recept på klassisk ugnspannkaka som toppas med vispad grädde och sylt. Den här ugnspannkakan</p>
+                    <a href="#" class="btn bg-GreenSheen">Läs mer</a>
+                </div>
+                <div class="post">
+                    <h2 class="text-GreenSheen">Renskavs gryta</h2>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Leo magna sodales diam sit lectus id etiam. Pellentesque scelerisque lacinia dictum fermentum sit</p>
+                    <a href="#" class="btn bg-GreenSheen">Läs mer</a>
+                </div>
+                <div class="post">
+                    <h2 class="text-GreenSheen">Spageti med köttfärs sås</h2>
+                    <p>Gör en extra stor sats av allas vardagsfavorit, spagetti och köttfärssås, och frys in. Vill du variera smaken, prova med lamm- eller viltfärs istället </p>
+                    <a href="#" class="btn bg-GreenSheen">Läs mer</a>
+                </div>
+                <div class="post">
+                    <h2 class="text-GreenSheen">Spageti med köttfärs sås</h2>
+                    <p>Gör en extra stor sats av allas vardagsfavorit, spagetti och köttfärssås, och frys in. Vill du variera smaken, prova med lamm- eller viltfärs istället </p>
+                    <a href="#" class="btn bg-GreenSheen">Läs mer</a>
+                </div>
+            </section>
         </main>
         <script src="js/index.js"></script>
     </body>
